f8a82d961d08c3b026ebddb349fa616bca6a61eb
Dustin Irving <dustin.irving@gmail.com>
2020-06-01 13:19:48 -0400

    Updates

---
+++ precache-manifest.400fb36db4580705bfacd7f2e8fd2f74.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47a8a2595ec50f6ea6a0c284b8f88f75",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ad54d095b0d0853500f4",
+    "url": "/portfolio/static/css/main.dd7b5f5a.chunk.css"
+  },
+  {
+    "revision": "fc97ecec8f5ee96820d8",
+    "url": "/portfolio/static/js/2.f99ef3bf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.f99ef3bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad54d095b0d0853500f4",
+    "url": "/portfolio/static/js/main.335b2147.chunk.js"
+  },
+  {
+    "revision": "7e84dee687ac6de54091",
+    "url": "/portfolio/static/js/runtime-main.4da689ad.js"
+  },
+  {
+    "revision": "72d648d64bb0c2aa6f814262a3c65751",
+    "url": "/portfolio/static/media/computer-bg.72d648d6.jpg"
+  },
+  {
+    "revision": "97fd3b6aaf44a0dbe7b4054cf9f3a9b7",
+    "url": "/portfolio/static/media/covid.97fd3b6a.gif"
+  },
+  {
+    "revision": "d0ac4c03997df0fb88adbe501ab631d0",
+    "url": "/portfolio/static/media/covid.d0ac4c03.jpg"
+  },
+  {
+    "revision": "25b3cafe0b3c484a123335ef49e8069a",
+    "url": "/portfolio/static/media/dustin.25b3cafe.jpg"
+  },
+  {
+    "revision": "20760f8eb115b91a0d2c0021052fc8dc",
+    "url": "/portfolio/static/media/employee.20760f8e.jpg"
+  },
+  {
+    "revision": "847375bd7ddf66de9ab52193b7d0f7d1",
+    "url": "/portfolio/static/media/employee.847375bd.gif"
+  },
+  {
+    "revision": "05b3029d6569ec86382fb5dae79895f4",
+    "url": "/portfolio/static/media/samaritan.05b3029d.gif"
+  },
+  {
+    "revision": "6c2a9d9b87fa85e419d56b35dcc824a4",
+    "url": "/portfolio/static/media/samaritan.6c2a9d9b.jpg"
+  },
+  {
+    "revision": "26033869066243f863c3b26ed98b3643",
+    "url": "/portfolio/static/media/travelbook.26033869.png"
+  },
+  {
+    "revision": "7abf72ce7db2aa631e571c69f909e0af",
+    "url": "/portfolio/static/media/travelbook.7abf72ce.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1afcc2f6f0a3a6d4d98d80ec65410b4a.js"
+  "/portfolio/precache-manifest.400fb36db4580705bfacd7f2e8fd2f74.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
