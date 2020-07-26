6e3d4e0fffce06fc5621dd95c536f41c42fba042
Dustin Irving <dustin.irving@gmail.com>
2020-07-26 12:35:30 -0400

    Updates

---
+++ precache-manifest.60f1dc4bad7de73f36ecd096314a8e67.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d329ea3c1bb4faef452290b77c01a37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8535ffaaf7b4c829de59",
+    "url": "/static/css/main.df123167.chunk.css"
+  },
+  {
+    "revision": "ede275d8224fd2577a5f",
+    "url": "/static/js/2.efb79bd2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.efb79bd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8535ffaaf7b4c829de59",
+    "url": "/static/js/main.2fcab6ed.chunk.js"
+  },
+  {
+    "revision": "a0ac1140309c735bc60b",
+    "url": "/static/js/runtime-main.6be4d7d3.js"
+  },
+  {
+    "revision": "9d506b4434d5c6a02b08f1e2e32b9eca",
+    "url": "/static/media/budget.9d506b44.gif"
+  },
+  {
+    "revision": "72d648d64bb0c2aa6f814262a3c65751",
+    "url": "/static/media/computer-bg.72d648d6.jpg"
+  },
+  {
+    "revision": "97fd3b6aaf44a0dbe7b4054cf9f3a9b7",
+    "url": "/static/media/covid.97fd3b6a.gif"
+  },
+  {
+    "revision": "d0ac4c03997df0fb88adbe501ab631d0",
+    "url": "/static/media/covid.d0ac4c03.jpg"
+  },
+  {
+    "revision": "25b3cafe0b3c484a123335ef49e8069a",
+    "url": "/static/media/dustin.25b3cafe.jpg"
+  },
+  {
+    "revision": "20760f8eb115b91a0d2c0021052fc8dc",
+    "url": "/static/media/employee.20760f8e.jpg"
+  },
+  {
+    "revision": "847375bd7ddf66de9ab52193b7d0f7d1",
+    "url": "/static/media/employee.847375bd.gif"
+  },
+  {
+    "revision": "e3ee4fb79d53b75dd374969cecc820b0",
+    "url": "/static/media/resume.e3ee4fb7.pdf"
+  },
+  {
+    "revision": "05b3029d6569ec86382fb5dae79895f4",
+    "url": "/static/media/samaritan.05b3029d.gif"
+  },
+  {
+    "revision": "6c2a9d9b87fa85e419d56b35dcc824a4",
+    "url": "/static/media/samaritan.6c2a9d9b.jpg"
+  },
+  {
+    "revision": "390aa1adeb91961d44d3feaaff69ed77",
+    "url": "/static/media/transactions.390aa1ad.jpg"
+  },
+  {
+    "revision": "26033869066243f863c3b26ed98b3643",
+    "url": "/static/media/travelbook.26033869.png"
+  },
+  {
+    "revision": "7abf72ce7db2aa631e571c69f909e0af",
+    "url": "/static/media/travelbook.7abf72ce.gif"
+  },
+  {
+    "revision": "6390a0368f1e5b831534394d1cfc072a",
+    "url": "/static/media/weather.6390a036.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.721dcc55896a5fc8b4a5723924bdb29f.js"
+  "/precache-manifest.60f1dc4bad7de73f36ecd096314a8e67.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
